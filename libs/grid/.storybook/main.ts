1de333e35596a7a2e68b9f7d5ac6c6479006ecd0
Yash Kundalia <yash01.developer@gmail.com>
2023-09-03 12:33:49 -0800

    chore(grid): added grid components to the storybook

--- libs/grid/.storybook/main.ts
+++ libs/grid/.storybook/main.ts
@@ -2,11 +2,7 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
-  ],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/angular',
     options: {},
